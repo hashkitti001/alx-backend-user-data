7f011d8d1ca51d934a740f9b230b6596e9f52b05
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-11 17:47:46 +0100

    define methods to check whether a route requires auth and checks if a request has an auth header

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 """ Module of Auth routes."""
+import re
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """A class for auth routes."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-         """Checks if a path requires authentication."""
-         return False
-    
+        """Checks if a path requires authentication."""
+
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                regex = ''
+                if excluded_paths[-1] == '*':
+                    regex = '{}.*'.format(excluded_paths[0:-1])
+                elif exclusion_path[-1] == '/':
+                    regex = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    regex = '{}/*'.format(exclusion_path)
+                if re.match(regex, path):
+                    return False
+            return True
+
     def authorization_header(self, request=None) -> str:
         """Gets the authorization header field from the request."""
+        if request is None:
+            return request.headers.get('Authorization', None)
         return None
-    
-    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """Gets the current user."""
