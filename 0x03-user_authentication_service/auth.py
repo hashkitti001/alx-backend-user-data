9d1d608309629b4bd477dffd04ae73a5dff83208
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-20 22:02:12 +0100

    feat: add method to create new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 """Module for doing auth stuff."""
 import bcrypt
-
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> bytes:
     """Hashes a plain text password with the Bluefish algo."""
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pwd
-de
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user in the auth db system."""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
+
+    
 
