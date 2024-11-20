3895853ad88f82a87e96935aa0081da46a4cc3e0
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-20 21:40:28 +0100

    feat: add update_user method to DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -35,16 +36,44 @@ class DB:
         try:
             new_user = User(email=email, 
                             hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
+            self.__session.add(new_user)
+            self.__session.commit()
         except Exception:
-            self._session.rollback()
+            self.__session.rollback()
             new_user = None
         return new_user
 
-    # def find_user_by(self, **kwargs):
-    #     """Returns a list of users filtered by a particular criteria."""
-    #     fields, values = [], []
-    #     for key, val in kwargs.items():
-    #         if hasattr(User, str):
-    #             pass
+    def find_user_by(self, **kwargs) -> User:
+        """Returns a list of users filtered by a particular criteria."""
+        fields, values = [], []
+        for key, val in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(val)
+            else:
+                raise InvalidRequestError()
+            result = self.__session.query(User).filter(
+                tuple_(*fields)._in([tuple(values)])
+            ).first()
+            if result is None:
+                raise NoResultFound()
+            return result
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user record with the aid of the find_user_by."""
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return None
+        update_src = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_src[getattr(User, key)] = value
+            else:
+                raise ValueError()
+            self.__session.query(User).filter(
+                User.id == user_id
+                ).update(
+                    update_src,
+                    synchronize_session=False
+                )
+
