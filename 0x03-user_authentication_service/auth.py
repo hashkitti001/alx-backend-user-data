880b38a4b6ed307ad9b248889f2227c6c3c26403
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-20 23:10:03 +0100

    add auth method for creating a user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,15 +6,18 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
 
+
 def _hash_password(password: str) -> bytes:
     """Hashes a plain text password with the Bluefish algo."""
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pwd
 
+
 def _generate_uuid() -> str:
     """Generates a UUID."""
     return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -30,7 +33,7 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
-    
+
     def valid_login(self, email: str, password: str) -> bool:
         """Validates a login attempt."""
         user = None
@@ -39,9 +42,20 @@ class Auth:
             if user is not None:
                 return bcrypt.checkpw(
                     password.encode("utf-8"),
-                     user.hashed_password
-                    )
+                    user.hashed_password
+                )
         except NoResultFound:
             return False
         return False
-    
+
+    def create_session(self, email: str) -> str:
+        """Creates a session for a logged in user."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if user is None:
+            return None
+        sess_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=sess_id)
+        return sess_id
