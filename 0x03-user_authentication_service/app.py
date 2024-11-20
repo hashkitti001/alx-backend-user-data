85a3515239f1dc396cd76754f7d72fac9912a0e6
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-20 22:55:57 +0100

    feat: add Flask endpoint to register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 """Module containing Flask app for defining endpoints."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route('/')
-def welcome():
+@app.route('/', methods=["GET"], strict_slashes=False)
+def welcome() -> str:
+    """GET /
+    Return:
+       - The home endpoint's payload.
+    """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """POST /users
+    Return: 
+        - The account creation payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email=email, password=password)
+        return jsonify({"email": email, "password": password})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
